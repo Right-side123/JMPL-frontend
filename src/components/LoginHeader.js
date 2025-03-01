5840c9713e512768fba48b459d483875cb41dc2d
Right-side123 <rohit.yadav@rightside.co.in>
2025-03-01 14:17:56 -0800

    150%

--- src/components/LoginHeader.js
+++ src/components/LoginHeader.js
@@ -4,10 +4,15 @@ import { NavLink } from 'react-router-dom';
 import 'font-awesome/css/font-awesome.min.css';
 
 import rightsideLogo from './Assets/rightsidelogo.svg';
+import locationicon from './Assets/locationIcon.png';
+import mailicon from './Assets/emailIcon.png';
+import whatsappicon from './Assets/whatsappicon.png'
+import callicon from './Assets/callIcon.png';
 
 const LoginHeader = () => {
     // const navigate = useNavigate();
     const [dateTime, setDateTime] = useState('');
+    const [supportModel, setSupportModel] = useState(false)
 
     useEffect(() => {
         const interval = setInterval(() => {
@@ -30,6 +35,10 @@ const LoginHeader = () => {
         return () => clearInterval(interval);
     }, []);
 
+    const handleSupportClick = () => {
+        setSupportModel(true);
+      };
+
     //   const handleLogout = () => {
     //     localStorage.removeItem('access_token');
     //     localStorage.removeItem('manager_id');
@@ -39,19 +48,60 @@ const LoginHeader = () => {
     //   };
 
     return (
-        <header className="header">
-            <div className="header_content">
-                <div className='header_firstdivlogo'>
-                    <NavLink to="https://www.rightside.co.in" target='blank' className="header_logo">
-                        <img src={rightsideLogo} alt='rightside_logo' className='header_rightside_logo' />
-                    </NavLink>
+        <div>
+            <header className="header">
+                <div className="header_content">
+                    <div className='header_firstdivlogo'>
+                        <NavLink to="https://www.rightside.co.in" target='blank' className="header_logo">
+                            <img src={rightsideLogo} alt='rightside_logo' className='header_rightside_logo' />
+                        </NavLink>
 
-                    <span className='date_time'>
-                        <i className="fas fa-clock"></i> {dateTime}
-                    </span>
+                        <span className='date_time'>
+                            <i className="fas fa-clock"></i> {dateTime}
+                        </span>
+                    </div>
+                    <div className='header_logout_container'>
+                        <button className="support_button"
+                            onClick={handleSupportClick} onMouseOver={() => setSupportModel(true)} onMouseOut={() => setSupportModel(false)}>Support</button>
+                       
+                    </div>
                 </div>
-            </div>
-        </header>
+            </header>
+
+            {supportModel && (
+                <div className='support_page'>
+                    <div className='Support_Container'>
+                        <div className='contact_details_container'>
+                            <h1 className='contactdetails_heading'>Contact Details</h1>
+                            <div className='location_details'>
+                                <img src={locationicon} alt='address' className='location_icon' />
+                                <span className='contact_details'>10th Floor, Aston tower, Sundervan Complex, <br />
+                                    Shashtri Nagar, Andheri West, Mumbai, 4000053, <br />
+                                    Maharashtra, India.</span><br />
+                            </div>
+                            <div>
+                                <img src={mailicon} alt='e-mail' className='mail_icon' />
+                                <span className="contact_email">
+                                    <NavLink to="mailto:helpdesk@rightside.co.in" className="contact_email">helpdesk@rightside.co.in</NavLink>
+                                </span><br />
+                            </div>
+                            <div className='phone_container'>
+                                <img src={whatsappicon} alt='call' className='call_icon' />
+                                <span className='contact_phone'>
+                                    <NavLink to="tel:9987324949" className="contact_phone">+91 99873 24949</NavLink>
+                                </span>
+                            </div>
+                            <div className='phone_container'>
+                                <img src={callicon} alt='landline' className='call_icon' />
+                                <span className='contact_phone'>
+                                    <NavLink to="tel:2268996899" className="contact_phone"> +91 22 6899 6899</NavLink>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </div>
     );
 };
 
